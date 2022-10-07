a37b18546e53096fcb7e7745f8b7747b0c4565e8
Cibele Martins <Cibele.Martins@ibm.com>
2022-10-07 10:28:30 -0300

    🚧 feat:  structure of folders ofcomponents organized and implementing parts these components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './components/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './components/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
+import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './components/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FooterComponent } from './components/footer/footer.component';
     RecipeItemComponent,
     RecipeDetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
