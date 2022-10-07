7a1f8162d91857ccc2cf6aa9de653743ab24f844
Cibele Martins <Cibele.Martins@ibm.com>
2022-10-07 14:06:39 -0300

    🚧 feat: Implementing recipe-list component

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipes } from '../recipes.molde';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipes[] = [
+    new Recipes('Macarrao', 'cozinhe o tomate com allho e sal', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.tudogostoso.com.br%2Fcardapios%2Freceitas-faceis%2Freceitas-de-macarrao-confira%2F&psig=AOvVaw2yGWbdNw5lIYnvtMtqOznG&ust=1665248557182000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjIlLfMzvoCFQAAAAAdAAAAABAE')
+  ]
 
   constructor() { }
 
