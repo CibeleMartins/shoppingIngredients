c93b21bb846ce51f775cc8fca1d8286bdd4bb28b
Cibele Martins <Cibele.Martins@ibm.com>
2022-10-25 10:35:11 -0300

    🚧 feat: Implemented dropdown directive

--- src/app/components/shared/directives/drop-down-directive.directive.ts
+++ src/app/components/shared/directives/drop-down-directive.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appDropDownDirective]'
 })
 export class DropDownDirectiveDirective {
 
+  @HostBinding('class') class: string = "btn-group";
+
   constructor() { }
+
+  @HostListener('click', ['$event.target']) onClick(evenData: Event) {
+
+    if(!this.class) {
+      this.class = "btn-group open"
+    } else {
+      this.class = ""
+    }
+  }
+
+
 
 }
