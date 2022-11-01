9d67c35dfa4840ae614132522a474509f9a7f8f8
Cibele Martins <Cibele.Martins@ibm.com>
2022-11-01 13:59:35 -0300

    🚧 feat: changed model of recipes

--- src/app/components/recipes/recipes.molde.ts
+++ src/app/components/recipes/recipes.molde.ts
@@ -1,13 +1,17 @@
+import { Ingredients } from "../shared/ingredients.model";
+
 export class Recipes {
 
     name: string;
     description:string;
     imagePath:string;
+    ingredients: Ingredients[];
 
-    constructor(name: string, descrip: string, imagePath:string) {
+    constructor(name: string, descrip: string, imagePath:string, ingredients: Ingredients[]) {
         
         this.name = name;
         this.description = descrip;
         this.imagePath = imagePath;
+        this.ingredients = ingredients
     }
 }
