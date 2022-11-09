98869ef6a95da72f8657aa11a33146e5f17990e6
Cibele Martins <Cibele.Martins@ibm.com>
2022-11-09 15:45:47 -0300

    🚧 feat: Implementando mais rotas aninhadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { RecipesComponent } from './components/recipes/recipes.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { SelectRecipeComponent } from './components/recipes/select-recipe/select-recipe.component';
 import {RecipeDetailComponent} from './components/recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './components/recipes/recipe-edit/recipe-edit.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/'},
   {path: 'recipes', component: RecipesComponent, children: [
     {path: '', component: SelectRecipeComponent},
     {path: ':id', component: RecipeDetailComponent},
+    {path: 'new', component: RecipeEditComponent},
+    {path: ':id/edit', component: RecipeEditComponent},
   ]},
   {path: 'shoppList', component: ShoppingListComponent}
 ];
